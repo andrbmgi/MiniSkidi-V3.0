b504e7aacdeb8db71534901786946d06687bd416
andrbmgi <andreas.raede@gmail.com>
2024-03-16 10:06:36 +0100

    Proportional control

--- MiniSkidi_3.0/MiniSkidi_3.0.ino
+++ MiniSkidi_3.0/MiniSkidi_3.0.ino
@@ -49,23 +49,35 @@
 #define ARMDOWN 6
 #define STOP 0
 
-#define RIGHT_MOTOR 1
-#define LEFT_MOTOR 0
+#define LEFT_MOTOR 1
+#define RIGHT_MOTOR 0
 #define ARM_MOTOR 2
 
 #define FORWARD 1
 #define BACKWARD -1
 
-#define MS_CENTER 990
-#define MS_MIN 200
-#define MS_MAX 1780
+#define MIN_PULSE_WIDTH       500     // the shortest pulse sent to a servo  
+#define MAX_PULSE_WIDTH      2500     // the longest pulse sent to a servo 
+
+#define MS_CENTER 992
+#define MS_MIN 172
+#define MS_MAX 1811
 #define DEAD_PERCENT 5
+#define DEAD_CENTER 12
 // global constants
+
+#define AUX_CENTER 992
+#define AUX_MIN 172
+#define AUX_MAX 1811
 
 extern const char* htmlHomePage PROGMEM;
 const char* ssid     = "ProfBoots MiniSkidi OG";
 
 // global variables
+
+// PWM properties
+const int freq = 50000;
+const int resolution = 8;
 
 Servo bucketServo;
 Servo auxServo;
@@ -89,110 +101,104 @@ std::vector<MOTOR_PINS> motorPins =
 {
   {25, 26},  //RIGHT_MOTOR Pins (IN1, IN2)
   {33, 32},  //LEFT_MOTOR  Pins
-  {21, 19}, //ARM_MOTOR pins
+  {21, 19},  //ARM_MOTOR pins
+};
+
+std::vector<MOTOR_PINS> motorPinsPWMChannel =
+{
+  {14, 15},  //RIGHT_MOTOR PWM Channel
+  {12, 13},  //LEFT_MOTOR  PWM Channel
+  {10, 11},  //ARM_MOTOR PWM Channel
 };
 
 AsyncWebServer server(80);
 AsyncWebSocket wsCarInput("/CarInput");
 
 long map_ms_percent(int ms_value) {
-  long percent_value = map(ms_value, MS_MIN, MS_MAX, 0, 100);
+  long percent_value = map(ms_value, MS_MIN, MS_MAX, -100, 100);
   return percent_value;
 }
 
-long map_analog(int ms_value) {
-  long servo_value = map(ms_value, MS_MIN, MS_MAX, 10, 180);
-  return servo_value;
+long map_ms_duty(int ms_value) {
+  long percent_value = map(ms_value, MS_MIN, MS_MAX, -255, 255);
+  return percent_value;
 }
 
-bool is_off(long value) {
-  return value > 50 - DEAD_PERCENT && value < 50 + DEAD_PERCENT;
+long map_aux(int ms_value) {
+  long servo_value = map(ms_value, AUX_MIN, AUX_MAX, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+  return servo_value;
 }
 
 void perform_remote_command(bfs::SbusData data) {
   long remote_throttle = map_ms_percent(data.ch[0]);
   long remote_steering = map_ms_percent(data.ch[1]);
-  long remote_arm = map_ms_percent(data.ch[2]);
-  long remote_bucket = map_analog(data.ch[3]);
-  long remote_aux = map_analog(data.ch[4]);
+  long remote_arm = map_ms_duty(data.ch[2]);
+  long remote_bucket = map_aux(data.ch[3]);
+  long remote_aux = map_aux(data.ch[4]);
   long remote_lights = map_ms_percent(data.ch[5]);
 
-  // Throttle
-  if (!is_off(remote_throttle)) {
-
-    if (is_off(remote_steering)) {
-      // Straight
-      if (remote_throttle > 50 + DEAD_PERCENT) {
-        // Forward
-        rotateMotor(RIGHT_MOTOR, FORWARD);
-        rotateMotor(LEFT_MOTOR, FORWARD);
-      } else if (remote_throttle < 50 - DEAD_PERCENT) {
-        // Backward
-        rotateMotor(RIGHT_MOTOR, BACKWARD);
-        rotateMotor(LEFT_MOTOR, BACKWARD);
-      }
-    } else {
-      // Curve
-      if (remote_throttle > 50 + DEAD_PERCENT) {
-        // Forward
-        if (remote_steering > 50 + DEAD_PERCENT) {
-          // Right
-          rotateMotor(RIGHT_MOTOR, STOP);
-          rotateMotor(LEFT_MOTOR, FORWARD);
-        } else if (remote_steering < 50 - DEAD_PERCENT) {
-          // Left
-          rotateMotor(RIGHT_MOTOR, FORWARD);
-          rotateMotor(LEFT_MOTOR, STOP);
-        }
-      } else if (remote_throttle < 50 - DEAD_PERCENT) {
-        // Backward
-        if (remote_steering > 50 + DEAD_PERCENT) {
-          // Right
-          rotateMotor(RIGHT_MOTOR, STOP);
-          rotateMotor(LEFT_MOTOR, BACKWARD);
-        } else if (remote_steering < 50 - DEAD_PERCENT) {
-          // Left
-          rotateMotor(RIGHT_MOTOR, BACKWARD);
-          rotateMotor(LEFT_MOTOR, STOP);
-        }
-      }
-    }
-  } else {
-    // No throttle
-    if (remote_steering > 50 + DEAD_PERCENT) {
-          // Turn right
-          rotateMotor(RIGHT_MOTOR, BACKWARD);
-          rotateMotor(LEFT_MOTOR, FORWARD);
-        } else if (remote_steering < 50 - DEAD_PERCENT) {
-          // Turn left
-          rotateMotor(RIGHT_MOTOR, FORWARD);
-          rotateMotor(LEFT_MOTOR, BACKWARD);
-        }
+  // dead zone when sticks are centered
+  if (remote_throttle > -DEAD_PERCENT && remote_throttle < DEAD_PERCENT) {
+    remote_throttle = 0;
+  }
+  if (remote_steering > -DEAD_PERCENT && remote_steering < DEAD_PERCENT) {
+    remote_steering = 0;
   }
 
-  if (remote_arm > 50 + DEAD_PERCENT) {
+  // input mixing
+  int leftThrottle = remote_throttle + remote_steering; 
+  int rightThrottle = remote_throttle - remote_steering;
+
+  // normalize if max > 100
+  float factor = 1.0;
+  if (leftThrottle > 100) {
+    factor = 100.0 / (float) leftThrottle;
+    leftThrottle = round(factor * leftThrottle);
+  }
+  if (rightThrottle > 100) {
+    factor = 100.0 / (float) rightThrottle;
+    rightThrottle = round(factor * rightThrottle); 
+  }
+
+  // map to duty cycle
+  leftThrottle = map( leftThrottle, -100, 100, -255, 255);
+  rightThrottle = map( rightThrottle, -100, 100, -255, 255);
+
+  //constrain
+  leftThrottle = constrain(leftThrottle, -255, 255);
+  rightThrottle = constrain(rightThrottle, -255, 255);
+
+
+  if (leftThrottle > 0) {
+    rotateMotor(LEFT_MOTOR, FORWARD, leftThrottle);
+  } else if (leftThrottle < 0) {
+    rotateMotor(LEFT_MOTOR, BACKWARD, abs(leftThrottle));
+  } else {
+    rotateMotor(LEFT_MOTOR, STOP, 0);
+  }
+
+  if (rightThrottle > 0) {
+    rotateMotor(RIGHT_MOTOR, FORWARD, rightThrottle);
+  } else if (rightThrottle < 0 - DEAD_CENTER) {
+    rotateMotor(RIGHT_MOTOR, BACKWARD, abs(rightThrottle));
+  } else {
+    rotateMotor(RIGHT_MOTOR, STOP, 0);
+  }
+
+  if (remote_arm > 0 + DEAD_CENTER) {
     // Lower
-    removeArmMomentum = true;
-    rotateMotor(ARM_MOTOR, FORWARD);
-  } else if (remote_arm < 50 - DEAD_PERCENT) {
+    rotateMotor(ARM_MOTOR, FORWARD, remote_arm);
+  } else if (remote_arm < 0 - DEAD_CENTER) {
     // Raise
-    rotateMotor(ARM_MOTOR, BACKWARD);
+    rotateMotor(ARM_MOTOR, BACKWARD, abs(remote_arm));
+  } else {
+    rotateMotor(ARM_MOTOR, STOP, 0);
   }
 
   bucketTilt(remote_bucket);
   auxControl(remote_aux);
 
-  if (is_off(remote_throttle) && 
-      is_off(remote_steering)) {
-    rotateMotor(RIGHT_MOTOR, STOP);
-    rotateMotor(LEFT_MOTOR, STOP);
-  } 
-
-  if (is_off(remote_arm)) {
-    rotateMotor(ARM_MOTOR, STOP);
-  }
-
-  if (remote_lights > 50) {
+  if (remote_lights > 0) {
     digitalWrite(lightPin1, HIGH);
     digitalWrite(lightPin2, LOW);
     light = true;
@@ -203,39 +209,26 @@ void perform_remote_command(bfs::SbusData data) {
   }
 }
 
-void rotateMotor(int motorNumber, int motorDirection)
+void rotateMotor(int motorNumber, int motorDirection, uint32_t speed)
 {
   if (motorDirection == FORWARD)
-  {
-    digitalWrite(motorPins[motorNumber].pinIN1, HIGH);
-    digitalWrite(motorPins[motorNumber].pinIN2, LOW);
+  { 
+    ledcWrite(motorPinsPWMChannel[motorNumber].pinIN1, speed);
+    ledcWrite(motorPinsPWMChannel[motorNumber].pinIN2, STOP);
   }
   else if (motorDirection == BACKWARD)
   {
-    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
-    digitalWrite(motorPins[motorNumber].pinIN2, HIGH);
+    ledcWrite(motorPinsPWMChannel[motorNumber].pinIN1, STOP);
+    ledcWrite(motorPinsPWMChannel[motorNumber].pinIN2, speed);
   }
   else
   {
-    if (removeArmMomentum)
-    {
-      digitalWrite(motorPins[ARM_MOTOR].pinIN1, HIGH);
-      digitalWrite(motorPins[ARM_MOTOR].pinIN2, LOW);
-      delay(10);
-      digitalWrite(motorPins[motorNumber].pinIN1, LOW);
-      digitalWrite(motorPins[motorNumber].pinIN2, LOW);
-      delay(5);
-      digitalWrite(motorPins[ARM_MOTOR].pinIN1, HIGH);
-      digitalWrite(motorPins[ARM_MOTOR].pinIN2, LOW);
-      delay(10);
-      removeArmMomentum = false;
-    }
-    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
-    digitalWrite(motorPins[motorNumber].pinIN2, LOW);
+    ledcWrite(motorPinsPWMChannel[motorNumber].pinIN1, STOP);
+    ledcWrite(motorPinsPWMChannel[motorNumber].pinIN2, STOP);
   }
 }
 
-void moveCar(int inputValue)
+void moveCar(int inputValue, uint32_t speed)
 {
   Serial.printf("Got value as %d\n", inputValue);
   if (!(horizontalScreen))
@@ -244,88 +237,87 @@ void moveCar(int inputValue)
     {
 
       case UP:
-        rotateMotor(RIGHT_MOTOR, FORWARD);
-        rotateMotor(LEFT_MOTOR, FORWARD);
+        rotateMotor(RIGHT_MOTOR, FORWARD, speed);
+        rotateMotor(LEFT_MOTOR, FORWARD, speed);
         break;
 
       case DOWN:
-        rotateMotor(RIGHT_MOTOR, BACKWARD);
-        rotateMotor(LEFT_MOTOR, BACKWARD);
+        rotateMotor(RIGHT_MOTOR, BACKWARD, speed);
+        rotateMotor(LEFT_MOTOR, BACKWARD, speed);
         break;
 
       case LEFT:
-        rotateMotor(RIGHT_MOTOR, BACKWARD);
-        rotateMotor(LEFT_MOTOR, FORWARD);
+        rotateMotor(RIGHT_MOTOR, BACKWARD, speed);
+        rotateMotor(LEFT_MOTOR, FORWARD, speed);
         break;
 
       case RIGHT:
-        rotateMotor(RIGHT_MOTOR, FORWARD);
-        rotateMotor(LEFT_MOTOR, BACKWARD);
+        rotateMotor(RIGHT_MOTOR, FORWARD, speed);
+        rotateMotor(LEFT_MOTOR, BACKWARD, speed);
         break;
 
       case STOP:
-        rotateMotor(ARM_MOTOR, STOP);
-        rotateMotor(RIGHT_MOTOR, STOP);
-        rotateMotor(LEFT_MOTOR, STOP);
+        rotateMotor(ARM_MOTOR, STOP, STOP);
+        rotateMotor(RIGHT_MOTOR, STOP, STOP);
+        rotateMotor(LEFT_MOTOR, STOP, STOP);
         break;
 
       case ARMUP:
-        rotateMotor(ARM_MOTOR, FORWARD);
+        rotateMotor(ARM_MOTOR, FORWARD, speed);
         break;
 
       case ARMDOWN:
-        rotateMotor(ARM_MOTOR, BACKWARD);
+        rotateMotor(ARM_MOTOR, BACKWARD, speed);
         removeArmMomentum = true;
         break;
 
       default:
-        rotateMotor(ARM_MOTOR, STOP);
-        rotateMotor(RIGHT_MOTOR, STOP);
-        rotateMotor(LEFT_MOTOR, STOP);
+        rotateMotor(ARM_MOTOR, STOP, STOP);
+        rotateMotor(RIGHT_MOTOR, STOP, STOP);
+        rotateMotor(LEFT_MOTOR, STOP, STOP);
         break;
     }
   } else {
     switch (inputValue)
     {
       case UP:
-        rotateMotor(RIGHT_MOTOR, BACKWARD);
-        rotateMotor(LEFT_MOTOR, FORWARD);
+        rotateMotor(RIGHT_MOTOR, BACKWARD, speed);
+        rotateMotor(LEFT_MOTOR, FORWARD, speed);
         break;
 
       case DOWN:
-        rotateMotor(RIGHT_MOTOR, FORWARD);
-        rotateMotor(LEFT_MOTOR, BACKWARD);
+        rotateMotor(RIGHT_MOTOR, FORWARD, speed);
+        rotateMotor(LEFT_MOTOR, BACKWARD, speed);
         break;
 
       case LEFT:
-        rotateMotor(RIGHT_MOTOR, BACKWARD);
-        rotateMotor(LEFT_MOTOR, BACKWARD);
+        rotateMotor(RIGHT_MOTOR, BACKWARD, speed);
+        rotateMotor(LEFT_MOTOR, BACKWARD, speed);
         break;
 
       case RIGHT:
-        rotateMotor(RIGHT_MOTOR, FORWARD);
-        rotateMotor(LEFT_MOTOR, FORWARD);
+        rotateMotor(RIGHT_MOTOR, FORWARD, speed);
+        rotateMotor(LEFT_MOTOR, FORWARD, speed);
         break;
 
       case STOP:
-        rotateMotor(ARM_MOTOR, STOP);
-        rotateMotor(RIGHT_MOTOR, STOP);
-        rotateMotor(LEFT_MOTOR, STOP);
+        rotateMotor(ARM_MOTOR, STOP, STOP);
+        rotateMotor(RIGHT_MOTOR, STOP, STOP);
+        rotateMotor(LEFT_MOTOR, STOP, STOP);
         break;
 
       case ARMUP:
-        rotateMotor(ARM_MOTOR, FORWARD);
+        rotateMotor(ARM_MOTOR, FORWARD, speed);
         break;
 
       case ARMDOWN:
-        rotateMotor(ARM_MOTOR, BACKWARD);
-        removeArmMomentum = true;
+        rotateMotor(ARM_MOTOR, BACKWARD, speed);
         break;
 
       default:
-        rotateMotor(ARM_MOTOR, STOP);
-        rotateMotor(RIGHT_MOTOR, STOP);
-        rotateMotor(LEFT_MOTOR, STOP);
+        rotateMotor(ARM_MOTOR, STOP, STOP);
+        rotateMotor(RIGHT_MOTOR, STOP, STOP);
+        rotateMotor(LEFT_MOTOR, STOP, STOP);
         break;
     }
   }
@@ -380,7 +372,7 @@ void onCarInputWebSocketEvent(AsyncWebSocket *server,
       break;
     case WS_EVT_DISCONNECT:
       Serial.printf("WebSocket client #%u disconnected\n", client->id());
-      moveCar(STOP);
+      moveCar(STOP, STOP);
       break;
     case WS_EVT_DATA:
       AwsFrameInfo *info;
@@ -397,7 +389,7 @@ void onCarInputWebSocketEvent(AsyncWebSocket *server,
         int valueInt = atoi(value.c_str());
         if (key == "MoveCar")
         {
-          moveCar(valueInt);
+          moveCar(valueInt, 255);
         }
         else if (key == "AUX")
         {
@@ -433,13 +425,19 @@ void onCarInputWebSocketEvent(AsyncWebSocket *server,
 
 void setUpPinModes()
 {
+  
 
   for (int i = 0; i < motorPins.size(); i++)
   {
     pinMode(motorPins[i].pinIN1, OUTPUT);
     pinMode(motorPins[i].pinIN2, OUTPUT);
+    ledcSetup(motorPinsPWMChannel[i].pinIN1, freq, resolution);
+    ledcSetup(motorPinsPWMChannel[i].pinIN2, freq, resolution);
+    ledcAttachPin(motorPins[i].pinIN1, motorPinsPWMChannel[i].pinIN1);
+    ledcAttachPin(motorPins[i].pinIN2, motorPinsPWMChannel[i].pinIN2);
   }
-  moveCar(STOP);
+
+  moveCar(STOP, STOP);
   bucketServo.attach(bucketServoPin);
   auxServo.attach(auxServoPin);
   auxControl(150);
@@ -448,7 +446,6 @@ void setUpPinModes()
   pinMode(lightPin1, OUTPUT);
   pinMode(lightPin2, OUTPUT);
 }
-
 
 void setup(void)
 {
